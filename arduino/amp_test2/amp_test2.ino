06971857bb4883a37facdd6e55e3c790a315f19a
Ivan Holub <ivanholub@gmail.com>
2015-02-12 21:36:09 +0200

    amp

---
+++ arduino/amp_test2/amp_test2.ino
@@ -0,0 +1,17 @@
+void setup() {
+
+Serial.begin(9600);
+}
+
+void loop() {
+
+float average = 0;
+for(int i = 0; i < 1000; i++) {
+average = average + (0.0740543262537397 * analogRead(A0) - 37.87878787878788);
+delay(1);
+}
+Serial.print(average);
+Serial.println(" mA");
+delay(300);
+}
+
